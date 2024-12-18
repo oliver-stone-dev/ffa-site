2df8be2eb454eabbbbdf6a86b96ff0473949cab6
oliver-stone-dev <oliver.stone.dev@outlook.com>
2024-12-18 13:59:26 +0000

    Report alert
    Terminal widgets now show latest report alerts

--- src/components/TerminalReportWidget.js
+++ src/components/TerminalReportWidget.js
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import '../styles.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faThumbsUp } from "@fortawesome/free-regular-svg-icons";
+import { faCaretUp } from "@fortawesome/free-solid-svg-icons";
+import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
 import { useContext } from "react";
 import { AccountContext } from "../context/AccountContext";
 import { ValidTokenContext } from "../context/ValidTokenContext";
@@ -26,9 +28,21 @@ const TerminalReportWidget = ({terminalToReport}) =>{
     const [scannerButtonChecked,setScannerChecked] = useState(false);
     const [checkButtonChecked,setCheckChecked] = useState(false);
     const [airportDataReports, setAirportDataReports] = useState(null);
-    const [userDataReports,setUserDataReports] = useState(null);
+    const [reportAlerts,setReportAlerts] = useState(null);
+    const [widgetOpen,setWidgetOpen] = useState(false);
 
     useEffect(() => {
+
+        fetch("http://localhost:5115/report/alerts?" + new URLSearchParams({
+            terminalId: terminal.id
+        }).toString())
+        .then((response) => {
+            if (!response.ok){
+                return null;
+            }
+            return response.json();
+        })
+        .then ((data) => setReportAlerts(data));
 
         if (tokenValid !== true || accountDetails === null){
             return;
@@ -121,18 +135,18 @@ const TerminalReportWidget = ({terminalToReport}) =>{
         );
     }
 
-    const ReportDataList = ({airportData}) =>{
+    const AirportDataList = ({airportData}) =>{
 
         if (airportData === null || airportData === undefined){
             return (
                 <div className="report-data-list">
-                    {WarningReport("Use caution")}
+                    {WarningReport("No official data, use caution!")}
                 </div>
             );
         }else if (airportData.length === 0){
             return (
                 <div className="report-data-list">
-                    {WarningReport("Use caution")}
+                    {WarningReport("No official data, use caution!")}
                 </div>
             );
         }
@@ -152,46 +166,90 @@ const TerminalReportWidget = ({terminalToReport}) =>{
         }
     }
 
+    const ReportDataList = ({airportData}) =>{
+
+        if (airportData === null || airportData === undefined){
+            return (
+                <div className="report-data-list">
+                    {OkayReport("Nothing to report.")}
+                </div>
+            );
+        }else if (airportData.length === 0){
+            return (
+                <div className="report-data-list">
+                    {OkayReport("Nothing to report.")}
+                </div>
+            );
+        }
+        else{
+        return(
+            <div className="report-data-list">
+                {airportData.map((report) => WarningReport(report.text))}
+            </div>
+        );
+        }
+    }
+
+    const onOpenPressed = () =>{
+        if (widgetOpen){
+            setWidgetOpen(false);
+        }else{
+            setWidgetOpen(true);
+        }
+    }
+
     const onReportPressed = (e,type) =>{
         if (e.target.value === "yes"){
             onReportClick(type);
         }
     }
 
-    return (
-        <div className="terminal-report-widget">
-            <div className="terminal-grid-container">
-                <div className="name-item"><h3>{terminal.name}</h3></div>
-                <div className="airport-data-item">
-                    <p>Official Data</p>
-                    <ReportDataList airportData={airportDataReports}></ReportDataList>
-                </div>
-                <div className="reports-item">
-                    <div className="our-reports-container">
-                        <img src={logoSmall} alt="small logo"></img>
-                        <p>Our Reports</p>
+    if (widgetOpen === false){
+            return (
+                <div className="terminal-report-widget-closed">
+                    <div className="terminal-grid-container">
+                        <div className="name-item"><h3>{terminal.name}</h3></div>
+                        <div onClick={onOpenPressed} className="expand-button-item"><FontAwesomeIcon icon={faCaretDown} /></div>
                     </div>
-                    <ReportDataList airportData={userDataReports}></ReportDataList>
                 </div>
-                <div className="make-report-item">
-                    <div className="scanner-report-container">
-                        <p>Have you spotted CT scanners at this terminals?</p>
-                        <div className="review-radio-buttons">
-                            <input checked={scannerButtonChecked} onChange={(event) => {onReportPressed(event,scannerReportType)}} id={terminal.name + "radio-btn-scanner"} type="radio" className="radio-input" name={terminal.name + "-scanner"} value="yes"></input>
-                            <label for={terminal.name + "radio-btn-scanner"} className="radio-button"><FontAwesomeIcon icon={faThumbsUp} />Yes</label>
+            );
+    }else{
+        return (
+            <div className="terminal-report-widget">
+                <div className="terminal-grid-container">
+                    <div className="name-item"><h3>{terminal.name}</h3></div>
+                    <div onClick={onOpenPressed} className="expand-button-item"><FontAwesomeIcon icon={faCaretUp} /></div>
+                    <div className="airport-data-item">
+                        <p>Official Data</p>
+                        <AirportDataList airportData={airportDataReports}></AirportDataList>
+                    </div>
+                    <div className="reports-item">
+                        <div className="our-reports-container">
+                            <img src={logoSmall} alt="small logo"></img>
+                            <p>Our Reports</p>
                         </div>
+                        <ReportDataList airportData={reportAlerts}></ReportDataList>
                     </div>
-                    <div className="check-report-container">
-                        <p>Have you been denied a hand-check?</p>
-                        <div className="review-radio-buttons">
-                            <input checked={checkButtonChecked} onChange={(event) => {onReportPressed(event,checksReportType)}} id={terminal.name + "radio-btn-checks"} type="radio" className="radio-input" name={terminal.name + "-checks"}value="yes"></input>
-                            <label for={terminal.name + "radio-btn-checks"} className="radio-button"><FontAwesomeIcon icon={faThumbsUp} />Yes</label>
+                    <div className="make-report-item">
+                        <div className="make-report-container">
+                            <p>Have you spotted CT scanners at this terminals?</p>
+                            <div className="review-radio-buttons">
+                                <input checked={scannerButtonChecked} onChange={(event) => {onReportPressed(event,scannerReportType)}} id={terminal.name + "radio-btn-scanner"} type="radio" className="radio-input" name={terminal.name + "-scanner"} value="yes"></input>
+                                <label for={terminal.name + "radio-btn-scanner"} className="radio-button"><FontAwesomeIcon icon={faThumbsUp} />Yes</label>
+                            </div>
+                        </div>
+                        <div className="make-report-container">
+                            <p>Have you been denied a hand-check?</p>
+                            <div className="review-radio-buttons">
+                                <input checked={checkButtonChecked} onChange={(event) => {onReportPressed(event,checksReportType)}} id={terminal.name + "radio-btn-checks"} type="radio" className="radio-input" name={terminal.name + "-checks"}value="yes"></input>
+                                <label for={terminal.name + "radio-btn-checks"} className="radio-button"><FontAwesomeIcon icon={faThumbsUp} />Yes</label>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default TerminalReportWidget;
