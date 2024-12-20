5f4c2a666c7cf2188806642dc37468bba5cdcd2a
oliver-stone-dev <oliver.stone.dev@outlook.com>
2024-12-20 17:06:08 +0000

    title, icon and about
    Changed title and icon
    
    Rewrote about page.

--- src/pages/About.js
+++ src/pages/About.js
@@ -47,44 +47,37 @@ const AboutPage = () => {
                 <div className="about-container">
                             <h1>Purpose of this site</h1>
                             <p>
-                                Despite the wide use of digital cameras and smartphones, film photography is making a comeback, big time.
+                                Despite the widespread use of digital cameras and smartphones, film photography is making a big comeback.
                             </p>
                             <p>
-                                And with air-travel more accessful and affordable then ever, it is becoming common for photographers to bring
-                                reels of film onto their flight.
+                                With air travel more accessible and affordable than ever, it has become increasingly common for photographers to bring rolls of film on their flights.
                             </p>
                             <p>
-                                The purpose of this site is a catalog and rate aiports on their accessbility and attitude towards film, and photographers
-                                carrying film. With a quick search, we want users to be able to see if the airport they are travelling to will be
-                                easy to get film through without damage.
+                                The purpose of this site is to catalog and rate airports based on their accessibility and attitude toward film, as well as their treatment of photographers 
+                                carrying film. With a quick search, we aim to help users determine if the airport they are traveling to will allow them to pass through security with their film undamaged.
                             </p>
                             <p>
-                                While we will try to keep the procedures and equipment of each airport current and up-to-date, it is the experieces of you, 
-                                the traveller that is most important. 
+                                While we attempt to keep information about airport procedures and equipment accurate and up-to-date, the experiences of you the traveller, are the most valuable resource. 
                             </p>
                             <p>
-                                So you happen to be travelling through security and see that they are using CT scanners or they dont allow hand-checks, please report it
-                                here, so other travellers are aware!
+                                So if you happen to go through security and notice that they are using CT scanners or do not allow hand checks, please report it here so we can help inform other travelers!
                             </p>
                     </div>
                     <div className="about-container">
                             <h1>Why should I be worred?</h1>
                             <p>
-                                Until recently, this was for the most part a simple matter, with standard x-ray machines not effecting film under ISO 800.
-                                However, with the introduction of powerful new scanners and stricter policies on hand-checking, taking film onto a flight is 
-                                not so easy.
-                            </p>
-                            <Link to="https://kodakprofessional.com/en-gb/photographers/resources/ct-scanning-x-ray-technology-and-film">Article by Kodak</Link>
-                            <p>
-                                These new scanners can dramatically reduce wait times at security, as travellers no longer need to remove items from their bag. Great!
+                                Until recently, travelling with film was a simple matter, as most standard x-ray machines do not affect film under ISO 800.
+                                However, with the introduction of powerful new scanners and stricter policies on hand-checking, taking film onto a flight has
+                                become more difficult.
                             </p>
                             <p>
-                                Now not all CT scanners are the same, so its not certain how your film would be effected if put through a CT scanner at your local airport.
-                                However, it seems to be the case that "
-                                <Link to="https://emulsive.org/articles/experiments/testing-ct-scanners-heres-how-badly-they-can-damage-your-film"> 
-                                these scanners lead to significantly fogged film, colour shifts, loss in shadow detail and a substantial increase in grain.
-                                </Link>"
+                                These new scanners can dramatically reduce wait times at security, as travellers no longer need to remove items from their bag. However,
+                                as stated in an article from 2021 
+                                "<Link to="https://emulsive.org/articles/experiments/testing-ct-scanners-heres-how-badly-they-can-damage-your-film"> 
+                                these scanners lead to significantly fogged film, colour shifts, loss in shadow detail and a substantial increase in grain."
+                                </Link>
                             </p>
+                            <Link to="https://kodakprofessional.com/en-gb/photographers/resources/ct-scanning-x-ray-technology-and-film">Article by Kodak warning about risks to film </Link>
                     </div>
                 </div>
             </div>
